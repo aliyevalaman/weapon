5db1bfa40ebfa102353e3415ecac294ad2fbc18c
aliyevalaman <tu7mpxu8u@code.edu.az>
2024-04-02 09:40:20 +0400

    lalala

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon gun = new Weapon(10, 5, false);
+
+            int choice = -1;
+            while (choice != 5)
+            {
+                Console.WriteLine("0 - İnformasiya almaq üçün");
+                Console.WriteLine("1 - Ateş açmaq üçün");
+                Console.WriteLine("2 - GetRemainBulletCount metodu üçün");
+                Console.WriteLine("3 - Reload metodu üçün");
+                Console.WriteLine("4 - ChangeFireMode metodu üçün");
+                Console.WriteLine("5 - Proqramdan dayandırmaq üçün");
+
+                Console.Write("Seçiminizi daxil edin: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("CapacityBullet: " + gun.CapacityBullet);
+                        Console.WriteLine("CurrentBullet: " + gun.CurrentBullet);
+                        Console.WriteLine("FireMode: " + (gun.FireMode ? "Avtomatik" : "Tekli"));
+                        break;
+                    case 1:
+                        gun.Shoot();
+                        break;
+                    case 2:
+                        int remainBulletCount = gun.GetRemainBulletCount();
+                        Console.WriteLine("Qalan güllə sayı: " + remainBulletCount);
+                        break;
+                    case 3:
+                        gun.Reload();
+                        break;
+                    case 4:
+                        gun.ChangeFireMode();
+                        break;
+                    case 5:
+                        Console.WriteLine("Proqram dayandırıldı.");
+                        break;
+                    default:
+                        Console.WriteLine("Yanlış seçim. Yenidən seçim edin.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Weapon
+    {
+        public byte CapacityBullet;
+        public byte CurrentBullet;
+        public bool FireMode;
+
+
+        public Weapon(byte CapacityBullet, byte CurrentBullet, bool fireMode)
+        {
+            this.CapacityBullet = CapacityBullet;
+            this.CurrentBullet = CurrentBullet;
+            this.FireMode = fireMode;
+        }
+
+        public void Shoot()
+        {
+            if(FireMode = false)
+            {
+                CurrentBullet= 1;
+                Console.WriteLine("Tekli ata bilersiz sadece");
+            }
+
+            else
+            {
+                CapacityBullet = 0;
+                Console.WriteLine("Darag boshdur");
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return CapacityBullet - CurrentBullet;
+           
+        }
+
+
+        public void Reload()
+        {
+            CapacityBullet = CurrentBullet;
+        }
+        
+        public void ChangeFireMode()
+        {
+            FireMode = !FireMode;
+            if (FireMode)
+            {
+                Console.WriteLine("Avtromotik");
+            }
+            else
+            {
+                Console.WriteLine("tekdir");
+            }
+        }
+
+       
+
+    }
+}
